6708020be6222d9400475de147bd443074a7c13e
tobibello <tobibello@DESKTOP-D29A86A.localdomain>
2020-03-01 16:57:35 -0400

    adding starting project

---
+++ script.js
@@ -0,0 +1,13 @@
+var page = prompt("how was our service today");
+
+function we () {if (page === bad){alert ("we are sorry")} else { alert ("pls indicate bad or good")};
+
+we()
+
+
+
+function calcArea (length, breadth){
+
+	breadth * length
+}
+
